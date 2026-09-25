cb3b52ae2bc16ad1abd3572ea5c6151b094e63eb
Agood-man <cigane2015stan@gmail.com>
2026-09-25 18:55:18 +0300

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-/*
- * Game bootstrap.
- * All runtime modules are loaded before this file from index.html.
- * Keeping this file tiny makes future bundling or further splitting easy.
- */
-window.AKTSPbGUT_DOOM_READY = true;
